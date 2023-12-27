d4c36a0afdf88deca94a72028a11da9344e9481d
sunil <sunil.n@hanatechpvt.com>
2023-12-27 11:49:14 +0530

    before deleting vendor and after creating entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -23,7 +23,7 @@ class Article
     private $title;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255,unique=true)
      */
     private $slug;
 
